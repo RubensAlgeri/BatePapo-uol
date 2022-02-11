7268d39d459de279d5d233b80f762b4b3c53d51f
Rubens Algeri <rubens_zys@hotmail.com>
2022-02-11 16:32:00 -0300

    enviar mensagem para o servidor

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,9 +1,17 @@
 let nomeUsuario;
 let promessaEnviada;
 let verificacao;
+let mensagens;
+let nomeUsuarioObjeto;
+let caixaMensagem;
+let mensagem;
+let mensagemAEnviar;
+let nomeDestinatario = "Todos";
+let mensagem1 = [];
+
 function pegarNome(){
     nomeUsuario = document.querySelector("aside input").value;
-    let nomeUsuarioObjeto = {
+    nomeUsuarioObjeto = {
     name: nomeUsuario
 }
     promessaEnviada = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', nomeUsuarioObjeto);
@@ -22,7 +30,37 @@ function liberarPagina(){
 function manterOnline(){
     verificarStatus = axios.post('https://mock-api.driven.com.br/api/v4/uol/status', nomeUsuarioObjeto)
 }
+let buscarMensagens = setInterval(pegarMensagens, 3000);
 function pegarMensagens(){
     mensagens = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
+    mensagens.then(classificarMensagem);
 }
-let buscarMensagens = setInterval(pegarMensagens, 3000);
+function classificarMensagem(mensagem){
+    mensagem1.push(mensagem);
+    let caixaMensagem = document.querySelector('main')
+    for(let i=0;i<mensagem1.length;i++){
+    if(mensagem1[i].type === 'status'){
+        caixaMensagem.innerHTML += `<article class="entrou-na-sala">
+        <p><em>${mensagem1[i].time}</em>  <strong>${mensagem1[i].from}</strong> para <strong>${mensagem1[i].to}</strong>:  ${mensagem1[i].text}</p>
+        </article>`;
+    }else if(mensagem1[i].type === 'message'){
+        caixaMensagem.innerHTML += `<article class="mensagem">
+        <p><em>${mensagem1[i].time}</em>  <strong>${mensagem1[i].from}</strong> para <strong>${mensagem1[i].to}</strong>:  ${mensagem1[i].text}</p>
+        </article>`;
+    }else if(mensagem1[i].type === 'private_message'){
+        caixaMensagem.innerHTML += `<article class="mensagem-reservada">
+        <p><em>${mensagem1[i].time}</em>  <strong>${mensagem1[i].from}</strong> para <strong>${mensagem1[i].to}</strong>:  ${mensagem1[i].text}</p>
+        </article>`;
+    }
+}
+}
+function enviarMensagem(){
+    mensagemAEnviar = {
+                from: nomeUsuario,
+                to: nomeDestinatario,
+                text: document.querySelector("footer input").value,
+                type: "message"
+    };
+    let mensagemEnviada = axios.post('https://mock-api.driven.com.br/api/v4/uol/messages', mensagemAEnviar);
+    mensagemEnviada.then()
+}
